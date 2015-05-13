6d140cc849d752c07011ce63aa5950ce67c67718
Daniel Harding <dharding@living180.net>
2015-05-13 15:38:44 +0100

    cleaner cache compatibility code

--- debug_panel/cache.py
+++ debug_panel/cache.py
@@ -1,12 +1,15 @@
-import django
 from django.core.cache.backends.base import InvalidCacheBackendError
 
+
 try:
-    if django.VERSION[:2] >= (1, 7):
-        from django.core.cache import caches
-        cache = caches['debug-panel']
-    else:
-        from django.core.cache import get_cache
-        cache = get_cache('debug-panel')
+    from django.core.cache import caches
+except ImportError:
+    from django.core.cache import get_cache
+else:
+    def get_cache(alias):
+        return caches[alias]
+
+try:
+    cache = get_cache('debug-panel')
 except InvalidCacheBackendError:
     from django.core.cache import cache
