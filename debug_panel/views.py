613aa291f3e962757977f1ed415c55815937a26a
Daniel Harding <dharding@living180.net>
2020-03-02 16:57:56 +0200

    replace deprecated render_to_response() call

--- debug_panel/views.py
+++ debug_panel/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from debug_panel.cache import cache
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.views.decorators.clickjacking import xframe_options_exempt
 
 @xframe_options_exempt
@@ -8,6 +8,6 @@ def debug_data(request, timestamp):
     html = cache.get("django-debug-panel:" + timestamp)
 
     if html is None:
-        return render_to_response('debug-data-unavailable.html')
+        return render(response, 'debug-data-unavailable.html')
 
     return HttpResponse(html, content_type="text/html; charset=utf-8")
